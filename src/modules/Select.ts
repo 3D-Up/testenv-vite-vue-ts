8663f76f4ab762161eaddf22d7241bd0c47c2062
Stefan-Biller <mail@stefan-biller.de>
2022-07-20 17:02:49 +0200

    comments

--- src/modules/Select.ts
+++ src/modules/Select.ts
@@ -5,7 +5,7 @@ export function updateSelection(
 ): boolean[] {
     // todo:
     // return a boolean for each circle: highlighted or not
-    // highlight every circle clockwise from the start Index to the current Index
+    // highlight every circle from the start Index clockwise to the current Index
 
     console.log({ current });
 
